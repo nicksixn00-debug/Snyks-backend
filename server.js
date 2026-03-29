b945057e71c6e6258d0f30e1bb08892d36941faa
nicksixn00-debug <nicksixn00@gmail.com>
2026-03-29 23:38:32 +0530

    Updated server.js with new express application code on 2026-03-29 18:07:47 UTC

--- server.js
+++ server.js
@@ -1,1 +1,59 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
 
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// 🔗 MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("DB Connected"))
+.catch(err => console.log(err));
+
+// 👤 User Schema
+const User = mongoose.model("User", {
+  username: String,
+  email: String,
+  password: String
+});
+
+// 📝 REGISTER
+app.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  const user = new User({
+    username,
+    email,
+    password: hashed
+  });
+
+  await user.save();
+
+  res.json({ message: "Registered successfully" });
+});
+
+// 🔐 LOGIN
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    return res.json({ message: "User not found" });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.json({ message: "Wrong password" });
+  }
+
+  res.json({ message: "Login successful" });
+});
+
+app.listen(3000, () => console.log("Server running"));
